5e28514de3cc48f7b79cd8e9dd2f64f58253163f
William Torrejon Celis <w.torrejon10@gmail.com>
2024-01-16 01:05:39 -0500

    Card modify

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,27 @@
 import IconLets from '../../assets/icon-lets.svg'
 
-const Card = () => {
+const Card = ({id,title, order, description, link}) => {
+
   return (
     <div className="wrapper-card flex items-center justify-center mt-4 p-10 bg-[#DAC5A7]/15 border-[#343230] ">
         <div className=" grid grid-cols-1 gap-10 ">
           <div className="data-text ">
             <div className='grid grid-cols-1 gap-1 '>
               <div className=" flex justify-start items-center">
-                <p className=" text-sm font-medium text-[#DAC5A799]">01</p>
+                <p className=" text-sm font-medium text-[#DAC5A799]">{order}</p>
               </div>
               <div>
-                <p className=" font-light text-2xl text-[#DAC5A7]">WEB DESING</p>
+                <p className=" font-light text-2xl text-[#DAC5A7]">{title}</p>
               </div>
             </div>
             <div>
-              <p className=" font-light leading-loose text-[#DAC5A7]">Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.</p>
+              <p className=" font-light leading-loose text-[#DAC5A7]">{description}</p>
             </div>
           </div>
           <div>
             <a className='w-70 flex justify-start items-center gap-4 cursor-pointer' href="">
               <img src={IconLets} alt="" />
-              <p className=" text-[#DAC5A7]">ABOUT WEB DESING</p>
+              <p className=" text-[#DAC5A7]">{link}</p>
             </a>
           </div>
         </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,12 +1,43 @@
 import Card from '../Card/Card'
 import './Cards.css'
+
+//array de objetos para renderizar dentro de las cards
+const dataCards = [
+ {
+    id: 1,
+    order: "01",
+    title: 'WEB DESING',
+    description: 'Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.',
+    link: 'ABOUT WEB DESING'
+  },
+  {
+    id: 2,
+    order: "02",
+    title: 'DEVELOPMENT',
+    description: 'Get custom web development solutions that are tailored to your specifications, designed to deliver a flawless user experience.',
+    link: 'ABOUT DEVELOPMENT'
+ },
+ {
+  id: 3,
+  order: "03",
+  title: 'CONTENT & SEO',
+  description: 'Proven SEO strategies that enhance your online performance, bringing you to the forefront of organic search results.',
+  link: 'ABOUT SEO'
+}
+]
+
+
 const Cards = () => {
   return (
     
-    <div className='flex justify-center items-center gap-4 px-12'>
-      <Card />
-      <Card />
-      <Card />
+    <div className='grid grid-cols-1 xl:grid-cols-3 gap-4 px-12'>
+       
+      {
+        dataCards.map((card) => (
+          <Card key={card.id} order={card.order} title={card.title} description={card.description} link={card.link} />
+        ))
+      }
+      
     </div>
   )
 }
