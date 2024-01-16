a02d0ab53ef3b6a5574bb4490bde5ff2d3e445ea
William Torrejon Celis <w.torrejon10@gmail.com>
2024-01-15 19:41:37 -0500

    add Cards

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,13 +1,16 @@
 import Navbar from '../../components/Navbar/Navbar.jsx'
+import Cards from '../../components/Cards/Cards.jsx'
 import './App.css'
 
+
+
+
 function App() {
-
-
   return (
     <>
       <div className='App flex-1'>
         <Navbar />
+        < Cards />
       </div>
 
 
