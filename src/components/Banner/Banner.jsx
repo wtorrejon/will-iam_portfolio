ad270be99cc2a6775d4e706ce7102dea071da2af
William Torrejon Celis <w.torrejon10@gmail.com>
2024-01-18 23:57:06 -0500

    create banner and font

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,17 +1,18 @@
 import banner from '../../assets/william.webp'
+import './Banner.css'
 
 const Banner = () => {
   return (
     <section className="Saction-hero w-full">
-        <div className="banner-container">
+        <div className="banner-container ">
             <div className="img-wrapper flex items-center justify-center">
                 <img src={banner} width='500px' alt="william" />
             </div>
-            <div className="flex justify-center items-center">
+            <div className="flex flex-col justify-center items-center">
                 <div>
-                    <div>
-                        <p>Web Designer</p>
-                        <p>& FrontEnd Developer</p>
+                    <div className='flex flex-col items-center justify-center'>
+                        <p className=' principalText text-[#DAC5A7] text-9xl'>Web Designer</p>
+                        <p className=' secondaryText text-[#DAC5A7] text-9xl' >& Developer</p>
                     </div>
                     <div>
                         <p></p>
@@ -25,6 +26,6 @@ const Banner = () => {
         </div>
     </section>
   )
-}
+}                                                                                                       
 
 export default Banner
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,42 +6,45 @@ import "./Navbar.css"
 const Navbar = () => {
 
   return (
-    <nav className='navbar-hero flex justify-between items-center  bg-[#272624] '>
-      <div className=" flex justify-center items-center p-4">
-        <div className="navbar-logo flex">
-          <img src={william} alt="logo" />
-        </div>
-        <div className="navbar-links pl-6 pr-6">
-          <ul className="navbar-links-links flex justify-between items-center gap-4" >
-            <li>
-              <a href="">SERVICES</a>
-            </li>
-            <li>
-              <a href="">WORK</a>
-            </li>
-            <li>
-              <a href="">ABOUT</a>
-            </li>
-            <li>
-              <a href="">BLOG</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-redes  bg-[#FFE70B] pr-3 pl-3 pt-2 pb-2 rounded-sm">
-          <ul className="navbar-redes-links flex justify-center items-center">
-            <li>
-              <a className=" text-[#272624]" href="letsTalk">
-                LET`S TALK
-              </a>
-            </li>
-          </ul>
-        </div>
 
+    <div className='fixed inset-x-0 top-0 flex justify-center items-center'>
+      <div className='navbar-hero justify-between items-center border-[#DAC5A7]/15 bg-[#DAC5A7]/5 '>
+        <div className=" flex justify-center items-center p-4">
+          <div className="navbar-logo flex">
+            <img src={william} alt="logo" />
+          </div>
+          <div className="navbar-links pl-6 pr-6">
+            <ul className="navbar-links-links flex justify-between items-center gap-4" >
+              <li>
+                <a href="">SERVICES</a>
+              </li>
+              <li>
+                <a href="">WORK</a>
+              </li>
+              <li>
+                <a href="">ABOUT</a>
+              </li>
+              <li>
+                <a href="">BLOG</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-redes  bg-[#FFE70B] pr-3 pl-3 pt-2 pb-2 rounded-sm">
+            <ul className="navbar-redes-links flex justify-center items-center">
+              <li>
+                <a className=" text-[#272624]" href="letsTalk">
+                  LET`S TALK
+                </a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+        <div className="burguer-icon">
+          <BurguerBotton />
+        </div>
       </div>
-      <div className="burguer-icon">
-        <BurguerBotton />
-      </div>
-    </nav>
+    </div>
   )
 }
 
