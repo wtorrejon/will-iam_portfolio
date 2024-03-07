3efb826bb195c635c02550c1b436b3f043d2d9a8
William Torrejon Celis <w.torrejon10@gmail.com>
2024-03-07 01:52:12 -0500

    add carousel infinite

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,6 +1,7 @@
 import Banner from '../../components/Banner/Banner.jsx'
 import Navbar from '../../components/Navbar/Navbar.jsx'
 import Cards from '../../components/cards/Cards.jsx'
+import Skills from '../../components/skills/Skills.jsx'
 
 import './App.css'
 
@@ -13,13 +14,10 @@ function App() {
       <div className='App flex-col justify-center items-center '>
         <Navbar />
         <Banner />
+        <Skills />
         <Cards />
       </div>
-
-
     </>
-
-
   )
 }
 
